aa6f59dbb5f172affe03ec8e63b66d3ece5b7808
parkchaee <yienie123411@naver.com>
2021-09-16 11:09:47 +0900

    초기 커밋.

---
+++ hw1/hw1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	printf("두 정수를 입력하세요.:");
+	scanf_s("%d, %d", &num1,&num2);
+	int num3 = num1 & num2;
+	printf("%d&%d=%d\n", num1, num2, num3);
+	int num4 = num1 | num2;
+	printf("%d|%d=%d\n", num1, num2, num4);
+	int num5 = num1 ^ num2;
+	printf("%d^%d=%d\n", num1, num2, num5);
+	return 0;
+
+}
